e3b3ef8959f50c5c25761de638471b09e1de39ff
DESKTOP-QHUCM82\frano <fran.otero.17@gmail.com>
2023-03-29 22:14:57 +0200

    Código orginal pero ya con nuestros parámetros geométricos

---
+++ Main.m
@@ -0,0 +1,62 @@
+%%
+
+clear all; close all;
+
+setPlot;
+%% MAIN PARAMETERS
+
+% Wing parameter definition
+
+Wing.Parameters.Chord     = 2.5;
+Wing.Parameters.AR        = 7;
+Wing.Parameters.Sweep     = 4.0856;
+Wing.Parameters.Sweepr    = deg2rad(Wing.Parameters.Sweep);
+Wing.Parameters.Sw        = 28;
+Wing.Parameters.lambda    = 0.6;
+Wing.Parameters.b         = (Wing.Parameters.AR*Wing.Parameters.Sw)^0.5;
+Wing.Parameters.dihedral  = 0;
+Wing.Parameters.dihedralr = deg2rad(Wing.Parameters.dihedral);
+
+Wing.Parameters.xoffset   = 0;            
+Wing.Parameters.yoffset   = 0;            
+Wing.Parameters.zoffset   = 0;            
+
+Wing.Parameters.prof_R  = '0000';
+Wing.Parameters.prof_T  = '0000';
+Wing.Parameters.PF      = 'trap';
+Wing.Parameters.tor_d   = -0.25;
+Wing.Parameters.theta0  = 1;
+Wing.Parameters.theta0r = deg2rad(Wing.Parameters.theta0);
+Wing.Parameters.tor     = @(y) Wing.Parameters.theta0r*(1-y^2/Wing.Parameters.b^2);
+
+Wing.Parameters.Nc      = 20;
+Wing.Parameters.nx      = Wing.Parameters.Nc+1;
+Wing.Parameters.Nss     = 30;
+Wing.Parameters.ny      = Wing.Parameters.Nss*2+1;
+Wing.Parameters.Nt      = Wing.Parameters.Nc*Wing.Parameters.Nss*2;
+Wing.Parameters.bias_x  = 1;
+Wing.Parameters.bias_y  = 1;
+
+% Flight Conditions
+
+FC.aoa     = [3];
+FC.aoar    = deg2rad(FC.aoa);
+FC.beta    = 0;
+FC.betar   = deg2rad(FC.beta);
+FC.M       = 0.3;
+FC.H       = 0;
+[T,a,P,rho]= atmosisa(FC.H);
+FC.U       = a*FC.M;
+FC.q       = 0.5*rho*FC.U^2;
+FC.naoa    = size(FC.aoar,2);
+
+%% Wing Generation
+
+[Wing]     = GenerateWing(Wing);
+[Wing]     = GenerateMesh(Wing);
+
+DisplayCase(Wing,FC);
+%% Vortex Lattice Simulation
+
+[Wing]     = VortexLattice(Wing,FC);
+
